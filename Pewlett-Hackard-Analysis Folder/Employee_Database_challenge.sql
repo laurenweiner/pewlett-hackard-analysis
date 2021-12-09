47665a7bea402e6f50b11957a0778f2206b5653d
laurenweiner <92737670+laurenweiner@users.noreply.github.com>
2021-12-09 18:03:50 -0500

    Update Employee_Database_challenge.sql

--- Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
@@ -40,3 +40,27 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+
+-- Deliverable 2
+-- Retrieve columns from Employees table 
+SELECT employees.emp_no, employees.first_name, employees.last_name, employees.birth_date,
+dept_employees.from_date, dept_employees.to_date
+INTO prep_table
+FROM employees
+LEFT JOIN dept_employees
+ON employees.emp_no = dept_employees.emp_no
+
+DROP TABLE mentorship_eligibility;
+
+SELECT DISTINCT ON (prep_table.emp_no) prep_table.emp_no, prep_table.first_name, prep_table.last_name, prep_table.birth_date,
+prep_table.from_date, prep_table.to_date, titles.title
+INTO mentorship_eligibility
+FROM prep_table
+LEFT JOIN titles 
+ON prep_table.emp_no = titles.emp_no
+WHERE (prep_table.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND prep_table.to_date = '9999-01-01'
+ORDER BY prep_table.emp_no;
+
+SELECT * FROM mentorship_eligibility; 
