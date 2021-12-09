549c1f8cd750963ca8c34f78fe78cc81d158c79e
laurenweiner <92737670+laurenweiner@users.noreply.github.com>
2021-12-09 15:01:51 -0500

    Update Employee_Database_challenge.sql

--- Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis Folder/Employee_Database_challenge.sql
@@ -34,3 +34,9 @@ ORDER BY emp_no, to_date DESC;
 
 SELECT * FROM unique_titles;
 
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT title, COUNT(title)
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
